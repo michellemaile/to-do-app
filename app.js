f28e7719f7b109072434d42dbe3035d9556b9d5d
Michelle Maile <michellemaile29@gmail.com>
2018-09-06 08:12:55 -0700

    checkpoint follow along

--- app.js
+++ app.js
@@ -1,57 +1,73 @@
-function onReady() {
-    const addToDoForm = document.getElementById('addToDoForm'); //form
-    const newToDoText =document.getElementById('newToDoText'); // text box
-    const toDoList = document.getElementById('toDoList'); // unordered list
+function onReady() {    
     
+    // create new variable named toDos and make it an array//
+    const toDos = [];
+
+    // create a new variable named addToDoForm which is a form <form id="addToDoForm"> (event listner below) //
+    const addToDoForm = document.getElementById('addToDoForm');
+
     
-    //when you click the button defined as <button type='submit' id='button'>Add To-Do!</button>, all this should happen
-    addToDoForm.addEventListener('submit', () => {
-        event.preventDefault();
+    //create new function called createNewToDo //
+    function createNewToDo() {
+    
+        //create new variable named newToDoText that grabs the element <label id=newToDoText> //
+        const newToDoText = document.getElementById('newToDoText');
         
-        //get the text -- why is it not TEXT because <input type="text" id= newToDoText> What is .value?
-        let title = newToDoText.value;
+//  //
+        if (!newToDoText.value) { return; }
+    
+        // call toDos (array that was defined above), push information to it: assign the value of the text input <form id="addToDoForm"> <input id=newToDoText> to the key "title" and create another key "complete" and initialize it to false //
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
+    
+        // clear the input (which HTML defined as text <input type="text") for the user by changing newToDoText to blank //
+        newToDoText.value='';
         
-        //create a new li defined by li
-        let newLi = document.createElement('li');
+        // call this function each time the state changes //
+        renderTheUI();
+    }
+    
+    // takes current state (toDos array) and render the UI
+    function renderTheUI() {
+        // create new variable called toDoList that grabs the element <ul id="toDoList"> //
+        const toDoList = document.getElementById('toDoList');
         
-        // create a checkbox defined by input
-        let checkbox = document.createElement('input');
+        // set newLi to empty string //
+        toDoList.textContent = '';
         
-        //set the checkbox's input's type to checkbox
-        checkbox.type = "checkbox";
-        
-        //create delete button defined by button
-        let deleteButton = document.createElement('button');
-            // create a text node
-        let t = document.createTextNode("Delete");   
-            // Append the text to <button>
-        deleteButton.appendChild(t);                           
-        
- ///////// tell the delete button to delete !!!!! This must be 'click,' not 'submit !!! Why?????? Is submit in reference to the fact that it takes input from somewhere whereas click is in reference to simply clicking on a button?
-        deleteButton.addEventListener('click', () => {
-            toDoList.removeChild(newLi);
+        //toDos is our array defined above. forEach() takes a function and applies it to each item in the array. In this case, render each to-do as a li in the ul //
+        toDos.forEach(function(toDo){
+            // create a new li defined by newLi //
+            const newLi = document.createElement('li');
+            // create a new input called checkbox //
+            const checkbox = document.createElement('input');
+            // checkbox is an input, set it's input type to checkbox //
+            checkbox.type = "checkbox";
             
-            });
-        
-        //set the title: the new Li (created above) needs text from "title" (associated with the text (value) from the input of newToDoText)
-        newLi.textContent = title;
-        
-        //attach the checkbox to the Li
-        newLi.appendChild(checkbox);
-        
-         // attach <deleteButton> to <newLi>
-        newLi.appendChild(deleteButton);  
-        
-        //attach the li to the ul
-        toDoList.appendChild(newLi);
-        
-        //empty the input
-        newToDoText.value ='';
-    });
+            // add the title text from toDo (defined when function was created) to newLi //
+            newLi.textContent = toDo.title;
+            
+            // add newLi to the toDoList //
+            toDoList.appendChild(newLi);
+            // add checkbox to the newLi //
+            newLi.appendChild(checkbox);
+        });
+    }
+    
+    //add event listener to the submit event <button type="submit"> of the form element <form id="addToDoForm"> When the button which was defined as a submit form button is pushed, this happens: //
+    addToDoForm.addEventListener('submit', event => {
+        // don't do the default reloading the page //                         
+        event.preventDefault();
+        // do the function "createNewToDo" defined above //
+        createNewToDo();                        
+    }); 
+    
+// why is this here twice? //    
+    renderTheUI();
 }
 
-//////// if you wanted to give the buttons seperate CSS properties, you'd have to add them selerately in HTML, right?
-
 window.onload = function() {
-    onReady();
+  onReady();
 };
